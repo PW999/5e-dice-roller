cbd1494edbfab9d1bf29536688d193cf7300efcb
Phillip <p_w999@ymail.com>
2020-12-24 13:09:17 +0100

    Made some methods private
    Learned about the private modified, it's of course much better than
    full public. Used the send method to call the private method which
    allows mock injection.
    
    Refactored some methods to be shorter, prefer guard clauses and unless
    statements where possible.

--- src/roll.rb
+++ src/roll.rb
@@ -20,9 +20,54 @@ class Roll
     set_disadvantage unless advantage
   end
 
+  ##
+  # Performs the roll of all dice, replacing rolls because of advantage or disadvantage if necessary
+  def execute
+    @dice.each do |die|
+      @rolls.push(die.roll)
+    end
+    @logger.debug("Rolls are #{@rolls}")
+    roll_extra_die
+    @rolls.sum
+  end
+
+  private
+
+  def roll_extra_die
+    return if @extra_die.nil? 
+
+    rolled_value = @extra_die.roll
+    @logger.debug("Extra roll was #{rolled_value}")
+    add_advantage(rolled_value) if @extra_die_advantage
+    add_disadvantage(rolled_value) unless @extra_die_advantage
+    @logger.debug("Rolls after (dis)advantage are #{@rolls}")
+  end
+
+  ##
+  # Replaces the lowest roll if the extra roll is higher than the lowest roll
+  def add_advantage(rolled_value)
+    @rolls = @rolls.sort
+    return if rolled_value < @rolls.at(0)
+
+    @logger.debug('Extra roll was more advantageous than the lowest roll, thus replacing it because of advantage')
+    @rolls.shift
+    @rolls.push(rolled_value)
+  end
+
+  ##
+  # Replaces the highest roll if the extra roll is lower than the highest roll
+  def add_disadvantage(rolled_value)
+    @rolls = @rolls.sort
+    return if rolled_value > @rolls.at((@rolls.size - 1))
+
+    @logger.debug('Extra roll was less advantageous than the higher roll, thus replacing it because of disadvantage')
+    @rolls.pop
+    @rolls.push(rolled_value)
+  end
+
   def set_advantage
     if !@extra_die.nil?
-      if !@extra_die_advantage   #if you already have disadvantage, then advantage cancels it out# 
+      unless @extra_die_advantage   # if you already have disadvantage, then advantage cancels it out
         @extra_die = nil
         @extra_die_advantage = nil
       end
@@ -34,7 +79,7 @@ class Roll
 
   def set_disadvantage
     if @extra_die.nil?
-      if @extra_die_advantage   #if you already have disadvantage, then advantage cancels it out# 
+      if @extra_die_advantage   # if you already have disadvantage, then advantage cancels it out
         @extra_die = nil
         @extra_die_advantage = nil
       end
@@ -42,35 +87,6 @@ class Roll
       @extra_die = Die.new(@sides)
       @extra_die_advantage = false
     end
-  end
-
-  def execute
-    @dice.each do |die|
-      @rolls.push(die.roll)
-    end
-    @rolls = @rolls.sort
-    @logger.debug("Rolls are #{@rolls}")
-    roll_extra_die
-    @rolls.sum
-  end
-
-  def roll_extra_die 
-    if !@extra_die.nil? 
-      extra_roll = @extra_die.roll
-      @logger.debug("Extra roll was #{extra_roll}")
-      if @extra_die_advantage
-        if extra_roll > @rolls.at(0)
-          @rolls.shift
-          @rolls.push(extra_roll)
-        end
-      else
-        if extra_roll < @rolls.at(@rolls.size() -1)
-          @rolls.pop
-          @rolls.push(extra_roll)
-        end
-      end
-    end
-    @logger.debug("Rolls after (dis)advantage are #{@rolls}")
   end
 
   def replace_dice(mock, extra_die)
--- test/tc_roll.rb
+++ test/tc_roll.rb
@@ -11,42 +11,42 @@ class TestRoll < Test::Unit::TestCase
 
   def test_roll_one_d20
     roll = Roll.new(1, 20, nil)
-    roll.replace_dice([MockDie.new(5)], nil)
+    roll.send('replace_dice', [MockDie.new(5)], nil)
     result = roll.execute
     assert_equal(result, 5, 'Expected single roll to be 5')
   end
 
   def test_roll_five_d20
     roll = Roll.new(5, 20, nil)
-    roll.replace_dice([MockDie.new(3), MockDie.new(1), MockDie.new(1), MockDie.new(15), MockDie.new(10)], nil)
+    roll.send('replace_dice', [MockDie.new(3), MockDie.new(1), MockDie.new(1), MockDie.new(15), MockDie.new(10)], nil)
     result = roll.execute
     assert_equal(result, 30, 'Expected sum of [3, 1, 1, 15, 10] rolls to be 30')
   end
 
   def test_roll_one_d20_with_advantage_success
     roll = Roll.new(1, 20, true)
-    roll.replace_dice([MockDie.new(3)], MockDie.new(10))
+    roll.send('replace_dice', [MockDie.new(3)], MockDie.new(10))
     result = roll.execute
     assert_equal(result, 10, 'Expected advantageous roll of 10 to be returned')
   end
 
   def test_roll_one_d20_with_advantage_fail
     roll = Roll.new(1, 20, true)
-    roll.replace_dice([MockDie.new(10)], MockDie.new(3))
+    roll.send('replace_dice', [MockDie.new(10)], MockDie.new(3))
     result = roll.execute
     assert_equal(result, 10, 'Expected original, higher roll of 10 to be returned')
   end
 
   def test_roll_one_d20_with_disadvantage_success
     roll = Roll.new(1, 20, false)
-    roll.replace_dice([MockDie.new(3)], MockDie.new(10))
+    roll.send('replace_dice', [MockDie.new(3)], MockDie.new(10))
     result = roll.execute
     assert_equal(result, 3, 'Expected disadvantageous roll of 3 to be returned')
   end
 
   def test_roll_one_d20_with_disadvantage_fail
     roll = Roll.new(1, 20, false)
-    roll.replace_dice([MockDie.new(10)], MockDie.new(3))
+    roll.send('replace_dice', [MockDie.new(10)], MockDie.new(3))
     result = roll.execute
     assert_equal(result, 3, 'Expected original, lower roll of 3 to be returned')
   end
