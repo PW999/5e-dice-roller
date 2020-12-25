21cc17e9626f020aafcdd5c602b1fdfbe3bc4a6c
Phillip <p_w999@ymail.com>
2020-12-25 10:28:56 +0100

    Implement parser
    The parser simple rolls the dice, replaces them in the string (frozen
    hence the .dup) and then uses Ruby's eval() to parse the math
    expression. A regex safeguards us from malicious eval expressions.

--- src/roll_parser.rb
+++ src/roll_parser.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'log_config'
+require_relative 'roll'
 
 ##
 # This class will interpret the text input and convert it into rolls of dice
@@ -14,24 +15,33 @@ class RollParser
   def parse
     raise MismatchedBracesError unless @text_roll.count('(') == @text_roll.count(')')
 
-    parse_block(@text_roll)
+    # eval is kind of evil so let's limit what it will take
+    rolled_string = replace_rolls @text_roll
+    raise EvalPreventionError unless (rolled_string =~ /[a-zA-Z_:,%\u0040-\uFFFF]/).nil?
+
+    evaluation_roll = eval(rolled_string)
+    @logger.info("#{@text_roll} => #{evaluation_roll}")
+    evaluation_roll
   end
 
   private
 
-  def parse_block(block)
-    @logger.debug("Parsing #{block}")
-  end
-
-  def split(block)
-    @logger.debug("Splitting #{block}")
-    
-  end
-
-  def rolls
-    @rolls
+  def replace_rolls(text_roll)
+    rolled_string = text_roll.dup        # the string should be non-frozen
+    die = rolled_string[/\d+D\d+/]
+    until die.nil?
+      parts = die.split 'D'
+      roll = Roll.new parts[0].to_i, parts[1].to_i, nil
+      rolled_string[die] = roll.execute.to_s
+      @logger.debug("#{die} rolled; #{text_roll} => #{rolled_string}")
+      die = rolled_string[/\d+D\d+/]
+    end
+    rolled_string
   end
 end
 
 class MismatchedBracesError < StandardError
+end
+
+class EvalPreventionError < StandardError
 end
--- test/tc_roll_parser.rb
+++ test/tc_roll_parser.rb
@@ -15,26 +15,37 @@ class TestRollParser < Test::Unit::TestCase
     assert_raise(MismatchedBracesError) { RollParser.new('(the rest of the content(does not matter)').parse }
   end
 
-  def test_split_one_die
-    result = @empty_parser.__send__('split', '1D20')
-    assert_equal ['+1D20'], result, 'Expected to find simple array'
+  def test_replace_one_die
+    rolled_output = @empty_parser.__send__ 'replace_rolls', '1D20'
+    assert(rolled_output['D'].nil?, 'All dice should have been rolled')
+    assert(!rolled_output[/(^[1-9])$|(1\d)|(20)/].nil?, 'The output should be a string between 1-20')
   end
 
-  def test_split_two_blocks
-    result = @empty_parser.__send__('split', '(1D20+2)+(1D6+1)')
-    assert_equal ['+(1D20+2)', '+(1D6+1)'], result, 'Expected to find two items in array'
+  def test_replace_multiple_dice
+    rolled_output = @empty_parser.__send__ 'replace_rolls', '1D6+1D6+1D4-1D8'
+    assert(rolled_output['D'].nil?, 'All dice should have been rolled')
+    assert(!rolled_output[/^[1-6]\+[1-6]\+[1-4]-[1-8]$/].nil?, 'The output should be a string matching [1-6]+[1-6]+[1-4]-[1-8]')
   end
 
-  def test_split_one_level
-    result = @empty_parser.__send__('split', '((1D20+2)+(1D6+1))-5')
-    assert_equal [['+(1D20+2)', '+(1D6+1)'], '-5'], result, 'Expected to find two items in array'
+  def test_replace_multiple_dice_with_braces
+    rolled_output = @empty_parser.__send__ 'replace_rolls', '1D6+(1D6+1D4)-1D8'
+    assert(rolled_output['D'].nil?, 'All dice should have been rolled')
+    assert(!rolled_output[/^[1-6]\+\([1-6]\+[1-4]\)-[1-8]$/].nil?, 'The output should be a string matching [1-6]+([1-6]+[1-4])-[1-8]')
   end
 
-  def skip_test_parse_simple_d20
-    parser = RollParser.new('1D20')
-    parser.parse
-    rolls = parser.__send__('rolls')
-    assert_equal 1, rolls.size, 'Expected exactly one roll'
+  def test_roll_two_d4
+    25.times do
+      rolled_value = RollParser.new('1D4+1D4').parse
+      assert rolled_value >= 2, "2D4 roll at least a 2, instead got #{rolled_value}"
+      assert rolled_value <= 8, "2D4 can not roll more than 8, instead got #{rolled_value}"
+    end
   end
 
+  def test_complex_roll
+    100.times do
+      rolled_value = RollParser.new('1D10+2*(1D20)-(1D4+5)').parse
+      assert rolled_value >= -6, "-6 is the lowest this roll can go but got #{rolled_value}"
+      assert rolled_value <= 46, "46 is the highest possible roll but got #{rolled_value}"
+    end
+  end
 end
